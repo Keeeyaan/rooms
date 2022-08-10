06c92db9a2fed84f328a3b7a647969960682fe40
Kean <keanjieden@gmail.com>
2022-08-11 00:25:38 +0800

    added working user register

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,9 @@
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
+import { setCredentials } from "../store/authSlice";
+import { useRegisterMutation } from "../api/apiSlice";
 
 import Page from "./Page";
 
@@ -14,19 +18,32 @@ import {
 
 const Register = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
+
+  const [register, { isLoading }] = useRegisterMutation();
+  const dispatch = useDispatch();
 
   const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const submitHandler = (e) => {
+  const submitHandler = async (e) => {
     e.preventDefault();
-
-    console.log(user);
-    console.log(isLoggedIn);
-    setUsername("");
-    setPassword("");
+    try {
+      const userData = await register({ username, email, password }).unwrap();
+      dispatch(setCredentials({ ...userData }));
+      setUsername("");
+      setPassword("");
+      navigate(from);
+    } catch (error) {
+      console.log(error);
+    }
   };
+
+  if (isLoading) {
+    <p>Loading...</p>;
+  }
 
   return (
     <Page maxWidth="xs" title="Login">
